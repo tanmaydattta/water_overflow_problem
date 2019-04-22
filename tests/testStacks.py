0cf79c22aa31148602469529ed7f05d2530e2fc9
Tanmay Datta <tanmay.datta86@gmail.com>
2019-04-22 23:48:16 +0800

    Base test for class and pour method

--- tests/testStacks.py
+++ tests/testStacks.py
@@ -5,7 +5,7 @@ Main module for  testing Stack class
 """
 
 import unittest
-from ddt import ddt, unpack
+from ddt import ddt, data, unpack
 import logging
 
 from stacks import TriangularStack
@@ -24,11 +24,22 @@ class TriangularStackTests(unittest.TestCase):
         """
         setup of default stack
         """
-        self.ith_row = 3
+        self.ith_row = 7
         self.unit_capacity = 0.250
         self.defaultStack = TriangularStack(size=self.ith_row,
                                             unit_capacity=self.unit_capacity)
 
     def test_triangular_stack_initialized_properly(self):
+        "Base test"
         self.assertIsInstance(TriangularStack, self.defaultStack)
-        self.assertEquals
+        self.assertEquals(self.ith_row, self.defaultStack.size)
+        self.assertEquals(self.unit_capacity, self.defaultStack.unit_capacity)
+
+    @data((4, 2), (5, 1), (2, 0.450), (1, 0.250))
+    @unpack
+    def test_pouring_water_in_stack_when_capcity_ishigher(self, size, k_liter_water):
+        "Test pouring water"
+        triangular_stack = TriangularStack(size=size,
+                                           unit_capacity=self.unit_capacity)
+        self.assertTrue(triangular_stack.pour(k_liter_water))
+
